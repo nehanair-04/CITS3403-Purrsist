958a5513ce2f7150e6f3f54ad5b3ddbd4c3ade84
NehaNair <nehanair04@gmail.com>
2026-05-04 20:18:49 +0800

    Fixed flask creation and files in app folder

--- app/routes.py
+++ app/routes.py
@@ -2,4 +2,6 @@ from flask import render_template, request, redirect, url_for, jsonify
 from app import app, db
 from app.models import Habit, HabitCompletion
 
-
+@app.route("/", methods=["GET"])
+def index():
+    return "Purrsist running!"
