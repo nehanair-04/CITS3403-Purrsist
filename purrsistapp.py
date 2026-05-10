06d2d6d5cb96ba9ae2ca12b17bb5e3e09d2ff9ee
NehaNair <nehanair04@gmail.com>
2026-05-11 00:50:27 +0800

    Fixes for bug in page visibility

--- purrsistapp.py
+++ purrsistapp.py
@@ -1,4 +1,9 @@
-from app import app
+from app import create_app, db
+from app.config import DeploymentConfig
+from flask_migrate import Migrate
+
+app = create_app(DeploymentConfig)
+migration = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run(debug=True)
